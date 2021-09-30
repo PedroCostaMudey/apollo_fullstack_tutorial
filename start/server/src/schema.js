365437fc1f9985205f6e6fd57306fe547e1edb4a
Pedro Costa <pedro.costa@gmail.pt>
2021-09-30 16:04:51 +0100

    gql pagination implementation

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -2,15 +2,38 @@ const { gql } = require("apollo-server");
 
 const typeDefs = gql`
   type Query {
-    launches: [Launch]!
+    launches(
+      """
+      The number of results tp show. Must be >= 1. Default = 20
+      """
+      pageSize: Int
+
+      """
+      If you add a cursor here, it will only return result _after_ this cursor
+      """
+      after: String
+      
+    ): LaunchConnection!
     launch(id: ID!): Launch
     me: User
   }
+
+  """
+  Simple wrapper around our list of launches that contains a cursor to the
+  last item in the list. Pass this cursor to the launches query to fetch results
+  after these.
+  """
 
   type Mutation {
     bookTrips(launchIds: [ID]!): TripUpdateResponse!
     cancelTrip(launchId: ID!): TripUpdateResponse!
     login(email: String): User
+  }
+
+  type LaunchConnection{ # add this below the Query type as an additional type.
+    cursor: String!
+    hasMore: Boolean!
+    launches: [Launch]!
   }
 
   type Launch {
