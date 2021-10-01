fcb44a5b1ef2f5d50aea0690d0eac407784bafc2
Pedro Costa <pedro.costa@gmail.pt>
2021-10-01 12:50:53 +0100

    frontend backend communication

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -22,13 +22,16 @@ const store = createStore();
 const server = new ApolloServer({
 
   context: async ({ req }) => {
-    // simple auth check on every request
+    //simple auth check on every request
     const auth = req.headers && req.headers.authorization || '';
+    
     const email = Buffer.from(auth, 'base64').toString('ascii');
     if (!isEmail.validate(email)) return { user: null };
+    
     // find a user by their email
     const users = await store.users.findOrCreate({ where: { email } });
     const user = users && users[0] || null;
+    
     return { user: { ...user.dataValues } };
   },
   typeDefs,
